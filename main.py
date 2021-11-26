6070ec6132fef377279321fea83e5de97f0fd58e
Samuel Sousa <samuel.lemobs@gmail.com>
2021-11-26 10:23:39 -0300

    data loading

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ import pandas as pd
 import numpy as np
 
 
+'''
+Dimensões da tabela fato :
+1 - NU_ANO(int) - ano do exame - (2017, 2018, 2019)
+2 - CO_ORGACAD(int - string) - codigo da categoria da faculdade
+3 - CO_GRUPO(int - string) - grupo do curso
+4 - CO_MODALIDADE(int - string) - modalidade de ensino
+5 - CO_UF_CURSO(int - string) - estado do curso
+6 - NU_IDADE(int) - idade
+7 - TP_SEXO(string) - sexo
+8 - CO_TURNO_GRADUACAO(int-string) - turno
+9 - NT_GER(double) - nota geral
+10 - CO_RS_I1(string) - grau de dificuldade
+11 - QE_I02(string) - etnia 
+'''
+
+
 def download_data(link, zip_name, save_path):
     data = requests.get(link)
 
@@ -65,50 +81,63 @@ def parse_nota(nota):
         return -1
     
 
+def is_NaN(data):
+    return str(data) == 'nan'
+
 def treat_data(row):
     return {
-        'ano' : row['NU_ANO'] if row['NU_ANO'] else 0,
-        'organizacao' : row['CO_ORGACAD'] if row['CO_ORGACAD'] else 0,
-        'grupo' : row['CO_GRUPO'] if row['CO_GRUPO'] else 0,
-        'modalidade' : row['CO_MODALIDADE'] if row['CO_MODALIDADE'] else -1,
-        'uf' : row['CO_UF_CURSO'] if row['CO_UF_CURSO'] else 0,
-        'idade' : row['NU_IDADE'] if row['NU_IDADE'] else 0,
-        'sexo' : row['TP_SEXO'] if row['TP_SEXO'] else 'NE',
-        'turno' : row['CO_TURNO_GRADUACAO'] if row['CO_TURNO_GRADUACAO'] else 0,
+        'ano' : row['NU_ANO'] if not is_NaN(row['NU_ANO']) else 0,
+        'organizacao' : row['CO_ORGACAD'] if not is_NaN(row['CO_ORGACAD']) else 0,
+        'grupo' : row['CO_GRUPO'] if not is_NaN(row['CO_GRUPO']) else 0,
+        'modalidade' : row['CO_MODALIDADE'] if not is_NaN(row['CO_MODALIDADE']) else -1,
+        'uf' : row['CO_UF_CURSO'] if not is_NaN(row['CO_UF_CURSO']) else 0,
+        'idade' : row['NU_IDADE'] if not is_NaN(row['NU_IDADE']) else 0,
+        'sexo' : row['TP_SEXO'] if not is_NaN(row['TP_SEXO']) else 'NE',
+        'turno' : row['CO_TURNO_GRADUACAO'] if not is_NaN(row['CO_TURNO_GRADUACAO']) else 0,
         'nota' : parse_nota(row['NT_GER']),
-        'dificuldade' : row['CO_RS_I1'] if row['CO_RS_I1'] and row['CO_RS_I1'] is not np.NaN else '-',
-        'etnia' : row['QE_I02'] if row['QE_I02'] and row['QE_I02'] is not np.NaN else '-'
+        'dificuldade' : row['CO_RS_I1'] if not is_NaN(row['CO_RS_I1']) else '-',
+        'etnia' : row['QE_I02'] if not is_NaN(row['QE_I02']) else '-'
     }
     
+
 '''
-Dimensões da tabela fato :
-1 - NU_ANO(int) - ano do exame - (2017, 2018, 2019)
-2 - CO_ORGACAD(int - string) - codigo da categoria da faculdade
-3 - CO_GRUPO(int - string) - grupo do curso
-4 - CO_MODALIDADE(int - string) - modalidade de ensino
-5 - CO_UF_CURSO(int - string) - estado do curso
-6 - NU_IDADE(int) - idade
-7 - TP_SEXO(string) - sexo
-8 - CO_TURNO_GRADUACAO(int-string) - turno
-9 - NT_GER(double) - nota geral
-10 - CO_RS_l1(string) - grau de dificuldade
-11 - QE_I02(string) - etnia 
+Extração de colunas que serão utilizadas. Facilita leituras posteriores dos arquivos e impacta menos na memória.
 '''
-def read_data():
+def preprocess_data():
+
+    columns = ['NU_ANO','CO_ORGACAD','CO_GRUPO', 'CO_MODALIDADE', 'CO_UF_CURSO', 'NU_IDADE', 'TP_SEXO', 'CO_TURNO_GRADUACAO', 'NT_GER', 'CO_RS_I1', 'QE_I02']
+
     data_2017 = pd.read_csv('./enade_2017/3.DADOS/MICRODADOS_ENADE_2017.txt', skipinitialspace=True, sep=';', low_memory=False)
     data_2018 = pd.read_csv('./enade_2018/2018/3.DADOS/microdados_enade_2018.txt', skipinitialspace=True, sep=';', low_memory=False)
     data_2019 = pd.read_csv('./enade_2019/3.DADOS/microdados_enade_2019.txt', skipinitialspace=True, sep=';', low_memory=False)
 
+    data_2017 = data_2017[columns]
+    data_2018 = data_2018[columns]
+    data_2019 = data_2019[columns]
+
+    data_2017.to_csv('enade_2017_preprocessed')
+    data_2018.to_csv('enade_2018_preprocessed')
+    data_2019.to_csv('enade_2019_preprocessed')
+
+
+def read_insert_data():
+
+    data_2017 = pd.read_csv('./enade_2017_preprocessed', skipinitialspace=True, sep=',', low_memory=False)
+    data_2018 = pd.read_csv('./enade_2018_preprocessed', skipinitialspace=True, sep=',', low_memory=False)
+    data_2019 = pd.read_csv('./enade_2019_preprocessed', skipinitialspace=True, sep=',', low_memory=False)
+
     sql_string = 'INSERT INTO Estudante(ano, organizacao, grupo, modalidade, uf, idade, sexo, turno, nota, dificuldade, etnia) VALUES '
     values = ''
-    for dataset in [data_2017.head(), data_2018.head(), data_2019.head()]:
-        for index, row in dataset.iterrows():
-            estudante = treat_data(row)
-            values += f'''  ({estudante['ano']},{estudante['organizacao']},{estudante['grupo']},
-                            {estudante['modalidade']},{estudante['uf']},{estudante['idade']},
-                            '{estudante['sexo']}',{estudante['turno']},{estudante['nota']},
-                            '{estudante['dificuldade']}', '{estudante['etnia']}' ),'''
+    with open('./test', 'w') as file:
+            for dataset in [data_2017, data_2018, data_2019 ]:
+                for index, row in dataset.iterrows():
+                    estudante = treat_data(row)
+                    values += f'''  ({estudante['ano']},{estudante['organizacao']},{estudante['grupo']},
+                                    {estudante['modalidade']},{estudante['uf']},{estudante['idade']},
+                                    '{estudante['sexo']}',{estudante['turno']},{estudante['nota']},
+                                    '{estudante['dificuldade']}', '{estudante['etnia']}' ),'''
 
+    
     sql_string += values[:-1] + ';'
     con = sqlite3.connect('estudante.db')
     cursor = con.cursor()
@@ -121,16 +150,18 @@ def read_data():
 def test_data():
     con = sqlite3.connect('estudante.db')
     cursor = con.cursor()
-    for row in cursor.execute('SELECT * FROM Estudante'):
-        print(row)
+    cursor.execute('SELECT * FROM Estudante LIMIT 100')
 
     con.commit()
     con.close()
 
-create_database()
-read_data()
+
+
+# Etapas iniciais, só precisam ser executadas uma vez.
+# preprocess_data()
+# create_database()
+
+read_insert_data()
 test_data()
-    
-    
-    
-        
+
+
