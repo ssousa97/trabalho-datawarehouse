5a26192232b721dd15c7109b11564cc284eeecc7
Samuel Sousa <samuel.lemobs@gmail.com>
2021-11-28 06:41:04 -0300

    modificacoes

--- aux.py
+++
@@ -1,8 +0,0 @@
-with open('./find_replace') as file:
-    values = ''
-    for line in file.readlines():
-        pair = line.split('=')
-        codigo = pair[0].strip()
-        nome = pair[1].strip()
-        values += f'''({codigo},'{nome}'),\n'''
-    print(values)
